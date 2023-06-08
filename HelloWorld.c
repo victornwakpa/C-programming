b7098f4babe56c4e934c8f37866e258235f3d51f
victornwakpa <nwakpavictor@gmail.com>
2023-06-08 13:36:50 +0100

    Update the HelloWorld.c file

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("My first C code");
-    return 0;
+    printf("My first C code \n"); // "\n" this is used to introduce a new line
+    printf("Another line of code!");
+    return 0; // the return statement returns 1 if error, then 0 if no error
 }
