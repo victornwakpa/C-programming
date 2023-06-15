2196c3a1c92d87d5216b3320b9b217ac17590119
victornwakpa <nwakpavictor@gmail.com>
2023-06-15 14:58:38 +0100

    This is a branch for escape sequence in C

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("My first C code \n"); // "\n" this is used to introduce a new line
-    printf("Another line of code!");
-    return 0; // the return statement returns 1 if error, then 0 if no error
+    // Escape sequence = character combination consisting of a backslash
+    // followed by a letter or combination of digits.
+    // They specify actions within a line or string of text.
+    // \n = newline
+    // \t = tab
+
+    printf("Hello World\nVictorious\n\n"); // \n is for adding a new line after a command is executed
+    printf("One \t Two \t Three\n\n"); //\t is for adding a tab
+    
+    //create a 3x3 matrics using Escape sequence
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
 }
+
+// Note that the escape character must be inside the qoute"" not outside it
