84a9e1057d5e1ee789aac35e094dfb2b5f24d43c
victornwakpa <nwakpavictor@gmail.com>
2023-06-20 14:11:16 +0100

    Added a file that accepts input from the user

---
+++ INPUTVALUE.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - The main function in this program
+* Description: This functions accepts a single value from a user
+*/
+int main()
+{
+    int ch;
+    printf("Please Enter a value: \n");
+    ch = getchar();
+    printf("The value you enter is: %c \n", ch);
+    return 0;
+}
