1595f7396910e3c562c2194a0f15bd67d6ebe06c
victornwakpa <nwakpavictor@gmail.com>
2023-06-20 13:21:13 +0100

    Added some files

---
+++ INT.c
@@ -0,0 +1,20 @@
+/**
+ * Integer can also be known as whole numbers because the only store numbers that doesnn't have decimal.
+ * If you want store or display numbers with decimals using int the decimal part will be cut off
+ */
+#include <stdio.h>
+
+int main(void) 
+{
+    int a;
+    int b;
+    int c;
+    
+    a = 12;
+    b = 13;
+    c = 14;  
+
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %d \n", b);
+    printf("The value of c is %d \n", c);
+}
